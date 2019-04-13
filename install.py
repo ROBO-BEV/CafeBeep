27cfb5f03c053af5de270fe4db045ab1ef43c9f3
Blaze Sanders <blaze@infinifill.com>
2019-04-13 11:34:04 -0700

    Started HTML coding

--- install.py
+++ install.py
@@ -6,4 +6,6 @@ sudo apt-get install espeak
 #http://pyqt.sourceforge.net/Docs/PyQt5/installation.html
 pip3 install pyqt5
 
-
+#https://aryaboudaie.com/python/technical/educational/web/flask/2018/10/17/flask.html
+#Remember to run flask with "python3" NOT "python" command, or you will get weird errors :)
+pip3 install flask
