f0a3c691f4edec4e7e8c859ea2b7ce541616e0f2
Blaze Sanders <blaze@infinifill.com>
2019-05-10 20:47:16 -0700

    LAST COMMIT BY BLAZE DOES NOT COMPILE FYI

--- CafeBeepKiosk-0.1/Drink.py
+++ CafeBeepKiosk-0.1/Drink.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 
 __author__ =  "Blaze Sanders"
@@ -11,40 +10,37 @@ __doc__ =     "Class to define valid drink configurtions for each kioks"
 # Useful system jazz
 import sys, time, traceback, argparse, string
 
-COLD_BREW = 1
-HEATED_COLD_BREW = 2
-HOT_DRIP_COFFEE = 3
-
-# AddOn Type Constants
-NONE = 0
-
-SUGAR_TYPE = -1
-SIMPLE_SYRUP = -11
-CARMEL = -12
-CHOCOLATE = -13
-
-MILK_TYPE = -2
-HALF_HALF = -21
-SOY = -22
-OATLY_XXX = -23
-
-ART_TYPE = -3
-NO_ART = -31
-BEEP_LOGO -32
-LEAF = -33
-ROBOT = -34
-N_A = -35
-
 class Drink:
+	# Drink Name Constants
+	COLD_BREW = 1
+	HEATED_COLD_BREW = 2
+	HOT_DRIP_COFFEE = 3
 
-	def __init__(self, drinkID, addOnTypes, addOnLevels):
-	def __init__(self, drinkID):
-		self.drinkName = getDrinkName(drinkID) 	#int(drinkID)
+	# AddOn Type Constants
+	NONE = 0
+
+	SUGAR_TYPE = -1
+	SIMPLE_SYRUP = -11
+	CARMEL = -12
+	CHOCOLATE = -13
+
+	MILK_TYPE = -2
+	HALF_HALF = -21
+	SOY = -22
+	OATLY_XXX = -23
+
+	ART_TYPE = -3
+	NO_ART = -31
+	BEEP_LOGO -32
+	LEAF = -33
+	ROBOT = -34
+	N_A = -35
+
+	def __init__(self, drinkName, addOnTypes, addOnLevels): 	#def __init__(self, drinkID):
+		self.drinkName = drinkName				#getDrinkName(int(drinkID))
 		self.AddOnTypes = addOnTypes
 		self.AddOnLevles = addOnLevels
 
-	def getDrinkName(drinkID):
-		return TODO SearchDrinkDatabase
 
 	def getSugarType(addOnTypes):
 		return self.addOnTypes[0]
@@ -56,7 +52,6 @@ class Drink:
 		return self.addOnTypes[2]
 
 
-
 	def getSugarLevel(addOnTypes):
 		return self.addOnLevel[0]
 
@@ -65,7 +60,14 @@ class Drink:
 
 
 	def createDrinkID():
+		print("TODO")
 
+        # TODO Define drinkID protocal - Thinking D.CSA9 where:
+        # D is drink name (1 = Cold Brew)
+        # C is cream level 0 to 8
+        # S is sugar level 0 to 8
+        # A is boolean latte art request (0 = NO ART, 1 to 8 is 8 different PNGs)
+        # 9 is end of line value to search for , so that more Add-Ons can be added to DrinkID protocal
 	def decodeDrinkID(addOnInfoRequested, drinkID)
 		if(addOnInfoRequested == SUGAR_TYPE):
 			return #TODO SUBSTRING FIRST DECIMAL POINT
@@ -83,7 +85,7 @@ class Drink:
 if __name__ = "__main":
 	addOnTypes = [NONE, HALF_HALF, "NO_ART.png"]
 	addOnLevels = [NONE, 3, N_A]
-	drinkForBlaze = Drink(COLD_BREW,addOnTypes, addOnLevels )
+	drinkForBlaze = Drink(COLD_BREW, addOnTypes, addOnLevels)
 	#drinkID = 1.0109039
 	#drinkForDavid = Drink(drinkID)
 
--- CafeBeepKiosk-0.1/UserData.py
+++ CafeBeepKiosk-0.1/UserData.py
@@ -13,29 +13,25 @@ __doc__ =     "Class to locally search user information, with data pulled and pu
 import sys, time, traceback, argparse, string
 
 
+
 class UserData:
 	MAX_USERS_PER_KIOSK = 4000 # Determine this limit via testing
+	AWS = -1
+	USB = -2
 
-	#TODO: What variable type should go in default database?
+	#TODO: What variable type should go in default database and how do I update the self.drinkObject?
 	userDatabase = {
-		0: Drink(, 1.1019, 0),
+		0: UserData("Blaze", 0),
 		1: "David",
 		2: "Murali"
 	}
-        lastDrink = NONE
-        favoriteDrinks = [NONE, NONE, NONE, NONE, NONE]
-	# TODO Define drinkID protocal - Thinking D.CSA9 where:
-	# D is drink name (1 = Cold Brew)
-	# C is cream level 0 to 8
-	# S is sugar level 0 to 8
-	# A is boolean latte art request (0 = NO ART, 1 to 8 is 8 different PNGs)
-	# 9 is end of line value to search for , so that more Add-Ons can be added to DrinkID protocal
-	def __init__(self, firstName, drinkID, userID):
+
+	def __init__(self, firstName, userID):
 		self.firstName = firstName
 		self.userID = userID
-		self.drinkObject = Drink(drinkID)
-		self.lastDrink = drinkOject.lastDrink
-		self.favoriteDrinks = drinkObject.favoriteDrinks #Array holding ??? number of drinks
+		self.drinkObject = Drink(Drink.NONE, [Drink.NONE, Drink.NONE, Drink.NONE], [0, 0])
+		self.lastDrink = Drink.NONE
+		self.favoriteDrinks = [Drink.NONE, Drink.NONE, Drink.NONE, Drink.NONE, Drink.NONE]
 
 	###
 	# Search user database (python Dictionary) to find user data
@@ -45,10 +41,16 @@ class UserData:
 	#
 	# @return - String variable with first name (only) of user. (PRIVACY MATTERS!)
 	###
-	def GetUserFirstName(userIdNum):
-		return self.userDatabase.get(userIdNum, " ")
+	def getUserFirstName(userIdNum):
+		return self.userDatabase.get(userIdNum, " ").firstName
 
+	def updateUserDatabase(source):
+		if(source == AWS):
+		elif(source == USB):
+		else:
+			print("INVLAID SOURCE FOR USER DATABASE UPDATE: USE AWS OR USB FLASHDRIVE")
 
 if __name__ == "__main__":
-	print("TEST")
+
+	print("END USERDATA MAIN")
 
