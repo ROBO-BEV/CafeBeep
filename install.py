3354b0b2257f4fc5f48b0efdcbf33bc84aa0dc52
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2020-02-13 14:55:28 -0800

    Added Python Virtual Environment Packaging Tool pipenv

--- install.py
+++ install.py
@@ -47,6 +47,9 @@ if __name__ == "__main__":
 
 	# Flask requires Python 3 to work
 	check_call("sudo apt install python3-pip", shell=True)
+	
+	# Use Python Virtual Environment Packaging Tool
+	check_call("pip install pipenv", shell=True)
 
 	# Flask is the GUI frontend to that runs in parallel with python backend controling pumps
 	# Remember to run flask with "python3" NOT "python" command, or you will get weird errors :)
